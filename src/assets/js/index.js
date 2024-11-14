11b2938f322e90813427fc40c0b0b9205534f614
acoji-luo <acoji.gm@gmail.com>
2024-11-14 14:56:52 +0300

    fixing the form section and creating a fixed menu

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,3 +1,13 @@
-import "../styles/reset.scss";
 import "../styles/mixins.scss";
+import "../styles/reset.scss";
 import "../styles/styles.scss";
+
+const header = document.getElementById("header");
+
+window.addEventListener("scroll", function () {
+	if (this.window.scrollY > 450) {
+		header.classList.add("header--fixed");
+	} else {
+		header.classList.remove("header--fixed");
+	}
+});
