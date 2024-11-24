d65acde450beb04a93e3fcc875b2bb9350de7145
acoji-luo <acoji.gm@gmail.com>
2024-11-24 13:27:24 +0300

    Image optimization and adaptive tweaks

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,74 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-	entry: {
-		app: "./src/assets/js/index.js",
-	},
-	output: {
-		clean: true,
-		filename: "[name].[contenthash].js",
-		path: path.resolve(__dirname, "dist"),
-	},
 	mode: "development",
-	devServer: {
-		static: "./src",
-		compress: true,
-		port: 3000,
-		hot: true,
+	entry: "./src/assets/js/index.js",
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: "bundle.js",
+		clean: true,
+	},
+	resolve: {
+		alias: {
+			images: path.resolve(__dirname, "src/assets/images/"),
+		},
+		extensions: [".js"],
 	},
 	module: {
 		rules: [
 			{
+				test: /\.(js)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: ["@babel/preset-env"],
+					},
+				},
+			},
+			{
+				test: /\.html$/,
+				use: "html-loader",
+			},
+			{
 				test: /\.(s[ac]ss|css)$/i,
 				//prettier-ignore
 				use: [
-					"style-loader",
-					"css-loader",
-					{
-						loader: "sass-loader",
-						options: {
-							api: "modern",
-							sassOptions: {}
-						}
-					}
-				],
+                    "style-loader",
+                    "css-loader",
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            api: "modern",
+                            sassOptions: {}
+                        }
+                    }
+                ],
 			},
 			{
-				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				test: /\.(gif|png|jpg|jpeg|svg)$/,
 				type: "asset/resource",
+				generator: {
+					filename: "assets/img/[name][ext][query]",
+				},
 			},
 		],
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
-			title: "YourTour",
-			template: "src/index.html",
+			template: "./src/index.html",
+			filename: "index.html",
+		}),
+		new MiniCssExtractPlugin({
+			filename: "styles.css",
 		}),
 	],
+	devServer: {
+		static: path.resolve(__dirname, "dist"),
+		port: 8000,
+		open: true,
+		hot: true,
+	},
 };
